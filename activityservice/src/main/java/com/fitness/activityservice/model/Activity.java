5f32e697fddd7205a025cc7c59705f5d81edca46
unknown <zshiva35@gmail.com>
2025-06-14 22:05:20 +0545

    Add Activity class and ActivityType enum in model

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -0,0 +1,29 @@
+package com.fitness.activityservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+@Document(collation = "activities")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Activity {
+    @Id
+    private String id;
+    private String userId;
+    private ActivityType type;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    private Map<String, Object> additionalMetrices;
+    private LocalDateTime createdTime;
+    private LocalDateTime updatedTime;
+
+}
