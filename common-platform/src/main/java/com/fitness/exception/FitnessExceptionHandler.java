3540d4eacba1410f4bbc9de324eece62775b9f9d
unknown <zshiva35@gmail.com>
2025-06-18 17:12:12 +0545

    Add handler for FitnessException for error response

--- common-platform/src/main/java/com/fitness/exception/FitnessExceptionHandler.java
+++ common-platform/src/main/java/com/fitness/exception/FitnessExceptionHandler.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class FitnessExceptionHandler {
     @ExceptionHandler(value = {Exception.class,FitnessException.class})
-    public ResponseEntity<?> handleException(FitnessException e){
-        String message = e.getFitnessErrorMessage().getMessage();
-        String code = e.getFitnessErrorMessage().getCode();
+    public ResponseEntity<?> handleException(FitnessException exception){
+        String message = exception.getFitnessErrorMessage().getMessage();
+        String code = exception.getFitnessErrorMessage().getCode();
         return ResponseEntity.badRequest().body(RestResponse.error(code,message));
     }
 }
