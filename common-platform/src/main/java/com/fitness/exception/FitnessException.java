81be5c86b36225a1bb3600e3d88863b242c33737
unknown <zshiva35@gmail.com>
2025-06-18 17:15:10 +0545

    Change in method object name

--- common-platform/src/main/java/com/fitness/exception/FitnessException.java
+++ common-platform/src/main/java/com/fitness/exception/FitnessException.java
@@ -3,9 +3,9 @@ package com.fitness.exception;
 public class FitnessException extends RuntimeException{
     private final FitnessErrorMessage fitnessErrorMessage;
 
-    public FitnessException(FitnessErrorMessage fitnessErrorMessage){
-        super(fitnessErrorMessage.getMessage());
-        this.fitnessErrorMessage = fitnessErrorMessage;
+    public FitnessException(FitnessErrorMessage errorMessage){
+        super(errorMessage.getMessage());
+        this.fitnessErrorMessage = errorMessage;
     }
     public FitnessErrorMessage getFitnessErrorMessage(){
         return fitnessErrorMessage;
